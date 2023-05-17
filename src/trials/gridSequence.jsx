50d86bed721443fcd0678768be379f73e60edff9
Exilic <tobias.engberg@exilic.se>
2023-05-18 00:14:33 +0200

    Single breakpoint, grid and sequence

---
+++ src/trials/gridSequence.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './gridSequence.css';
+
+const GridSequence = () => {
+    return (
+        <div id="container">
+            <article>
+                <img src="/img/garden.png" alt="garden" />
+                <h2>Consectetur adipisicing elit. Eligendi ipsum nihil odio?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consequuntur exercitationem magni optio quam repudiandae sequi temporibus! Alias autem, consectetur enim eos eum excepturi facere, incidunt minima natus odio quam, quia sint! Error, temporibus.</p>
+            </article>
+            <article>
+                <img src="/img/road.png" alt="road" />
+                <h2>Adipisci dolor error et iusto magni nesciunt nulla similique?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consequuntur exercitationem magni optio quam repudiandae sequi temporibus! Alias autem, consectetur enim eos eum excepturi facere, incidunt minima natus odio quam, quia sint! Error, temporibus.</p>
+            </article>
+            <article>
+                <img src="/img/villa.png" alt="villa" />
+                <h2>Laborum nobis nostrum, numquam quas quos repudiandae voluptatem.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consequuntur exercitationem magni optio quam repudiandae sequi temporibus! Alias autem, consectetur enim eos eum excepturi facere, incidunt minima natus odio quam, quia sint! Error, temporibus.</p>
+            </article>
+            <article>
+                <img src="/img/walk.png" alt="walk" />
+                <h2>Nihil provident saepe sed, voluptatem. Commodi, fugiat, vel!</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consequuntur exercitationem magni optio quam repudiandae sequi temporibus! Alias autem, consectetur enim eos eum excepturi facere, incidunt minima natus odio quam, quia sint! Error, temporibus.</p>
+            </article>
+        </div>
+    );
+};
+
+export default  GridSequence;
